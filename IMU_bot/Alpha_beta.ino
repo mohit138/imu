a1bcc1cc018f94fcf9bac774ec81804e92d2f8a6
mohit138 <39694943+mohit138@users.noreply.github.com>
2018-10-09 00:54:31 +0530

    MiniImuAHRS has the code for only imu readings.

---
+++ IMU_bot/Alpha_beta.ino
@@ -0,0 +1,19 @@
+void alphabeta_init(Alphabeta *pyaw, float Alpha,float Beta,float THeta,float Rk ,float THeta_previous ,float THetaintegrate ,float THetaintegrate_previous){
+ pyaw->alpha=Alpha;
+ pyaw->beta=Beta;
+ //pyaw->Theta=THeta;
+ pyaw->rk=Rk;
+ pyaw->Theta_previous=THeta_previous;
+ //pyaw->Thetaintegrate=THetaintegrate;
+ pyaw->Thetaintegrate_previous=THetaintegrate_previous;
+}
+
+void alphabeta_update(Alphabeta *pyaw,float measured_value){
+    pyaw->Theta = pyaw->Theta_previous + ( pyaw->Thetaintegrate_previous * Time );
+    pyaw->Thetaintegrate = pyaw->Thetaintegrate_previous;
+    pyaw->rk = measured_value - pyaw->Theta;
+    pyaw->Theta += pyaw->alpha * pyaw->rk;
+    pyaw->Thetaintegrate += ( pyaw->beta * pyaw->rk )/Time;
+    pyaw->Theta_previous = pyaw->Theta;
+    pyaw->Thetaintegrate_previous = pyaw->Thetaintegrate;  
+}
---
+++ IMU_bot/Kalman_FILTER.ino
@@ -0,0 +1,20 @@
+
+float qq,rr,pp,initial_estimate;
+
+
+void kalman_init(Kalman *kf,float qq,float rr,float pp,float initial_estimate){
+  kf->q=qq;                          // kf kalman struct
+                                     // x is estimate of sensor reading or input for the filter
+  kf->r=rr;                           
+  kf->p=pp;
+  kf->x=initial_estimate;
+}
+
+void update_kalman(Kalman *kf,float measured_value){
+
+  kf->p=kf->p+kf->q;                        //r is the error in measurement
+  kf->k=kf->p/(kf->p + kf->r);              // k is the kalman gain
+  kf->x=kf->x+kf->k*(measured_value-kf->x); //p is the error in estimate
+  kf->p=(1-kf->k)*kf->p;                    //q process noise covariance
+}
+
